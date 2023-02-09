c4aa4d418fab4aba140471ecb25fc615e50f9307
Tristin Sorrells <Tristinsorrells1@gmail.com>
2023-02-09 16:56:53 -0600

    Add loading message

--- src/App/App.js
+++ src/App/App.js
@@ -47,37 +47,42 @@ class App extends React.Component {
     return (
       <main>
         <Navbar />
-        <Route 
-          exact path="/"
+        <Route
+          exact
+          path="/"
           render={() => {
-            if (this.state.movies && !this.state.loading) { 
-              return (
-                <MovieContainer
-                  movies={this.state.movies}
-                />
-              )
+            if (this.state.movies && !this.state.loading) {
+              return <MovieContainer movies={this.state.movies} />;
             }
           }}
         />
         <Route
-          exact path="/:id"      
-          render={({match}) => {
-              // this.selectMovie(match.params.id)
-  
-              return (
-                <MovieDetails
-                  movie={this.state.selectedMovie}
-                  selectMovie={this.selectMovie}
-                  matchID={match.params.id}
-                  removeSelectedMovie={this.removeSelectedMovie}
-                />
-              );
+          exact
+          path="/:id"
+          render={({ match }) => {
+            // this.selectMovie(match.params.id)
+
+            return (
+              <MovieDetails
+                movie={this.state.selectedMovie}
+                selectMovie={this.selectMovie}
+                matchID={match.params.id}
+                removeSelectedMovie={this.removeSelectedMovie}
+              />
+            );
           }}
         />
         {this.state.error && (
           <h2 className="error-message">
             Sorry - We are having server issues. Please try again later.
           </h2>
+        )}
+        {this.state.loading && (
+          <section>
+            <h2 className="loading">
+              Loading ...
+            </h2>
+          </section>
         )}
       </main>
     );
