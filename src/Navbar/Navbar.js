d7291157520fbbcf93e33525295e9d8fc0af25c6
Rick Vermeil <rickv85@gmail.com>
2023-02-11 09:18:22 -0700

    Add link to site title

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,16 +1,19 @@
 import React from 'react'
 import './Navbar.css'
 import Popcorn from "../Popcorn.png";
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
 
 
     return (
       <header className="header">
-        <div className="header-container">
-          <h1 className="site-name">Funky Flix</h1>
-          <img className="popcorn-icon" src={Popcorn} alt="popcorn icon"></img>
-        </div>
+        <NavLink to={`/`} className="site-title-link">
+          <div className="header-container">
+            <h1 className="site-name">Funky Flix</h1>
+            <img className="popcorn-icon" src={Popcorn} alt="popcorn icon"></img>
+          </div>
+        </NavLink>
       </header>
     )
 
